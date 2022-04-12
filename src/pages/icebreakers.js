eb1950eca71fd6fda42be7a5e852da43bc278606
achristmascarl <carl.z.liu@gmail.com>
2022-04-11 20:03:29 -0400

    remove layout from icebreakers page

--- src/pages/icebreakers.js
+++ src/pages/icebreakers.js
@@ -16,44 +16,29 @@ export default function Home() {
   }
 
   return (
-    <Layout icebreakers icon={pageIcon}>
-      <Head>
-        <link
-          rel="icon"
-          href={
-            'data:image/svg+xml,' +
-            '<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>' +
-              `<text y=%22.9em%22 font-size=%2290%22>${pageIcon}</text>` +
-            '</svg>'
-          }
-        />
-        <title>{pageTitle}</title>
-        <meta name="og:title" content={pageTitle} />
-      </Head>
+    <div
+      className={c(
+        'max-w-7xl',
+        'md:h-screen',
+        'mx-auto',
+        'px-4',
+        'sm:px-6',
+        'lg:px-8',
+      )}
+    >
       <div
-          className={c(
-            'max-w-7xl',
-            'md:h-screen',
-            'mx-auto',
-            'px-4',
-            'sm:px-6',
-            'lg:px-8',
-          )}
-        >
-          <div
-            className={c(
-              'p-5',
-              'mx-auto',
-              'max-w-2xl',
-              'flex',
-              'flex-col',
-              'justify-center',
-              'text-center',
-            )}
-          >
-            {getIcebreaker()}
-          </div>
-        </div>
-    </Layout>
+        className={c(
+          'p-5',
+          'mx-auto',
+          'max-w-2xl',
+          'flex',
+          'flex-col',
+          'justify-center',
+          'text-center',
+        )}
+      >
+        {getIcebreaker()}
+      </div>
+    </div>
   )
 }

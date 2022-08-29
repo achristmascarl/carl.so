78bfeff7529a1a825822569fc0ffd2728a314ba9
achristmascarl <carl.z.liu@gmail.com>
2022-08-29 10:25:55 -0400

    adjust gtag implementation based on stackoverflow

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -6,15 +7,19 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* <!-- Google tag (gtag.js) --> */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-7C5JKWZTHL"></script>
-          <script>
-            {() => {
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-7C5JKWZTHL"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
+
               gtag('config', 'G-7C5JKWZTHL');
-            }}
-          </script>
+            `}
+          </Script>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href={

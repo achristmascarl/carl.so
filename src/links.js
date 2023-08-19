7fd4ba7cad8404c17dcdd5f7498cc0a125705679
achristmascarl <carl.z.liu@gmail.com>
2023-08-18 21:19:05 -0400

    move redirects to root, add active/inactive flag

---
+++ src/links.js
@@ -0,0 +1,23 @@
+export const links = {
+  'linkedin': {
+    id: 'linkedin',
+    url: 'https://www.linkedin.com/in/achristmascarl/',
+    name: 'LinkedIn',
+    redirectActive: true,
+    menuLink: true,
+  },
+  'twitter': {
+    id: 'twitter',
+    url: 'https://twitter.com/achristmascarl',
+    name: 'Twitter',
+    redirectActive: true,
+    menuLink: false,
+  },
+  'github': {
+    id: 'github',
+    url: 'https://github.com/achristmascarl',
+    name: 'GitHub',
+    redirectActive: true,
+    menuLink: true,
+  },
+};

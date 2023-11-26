f5c43a00a03904aaf89f5543bf93e2f0323e51b9
achristmascarl <carl.z.liu@gmail.com>
2023-11-26 01:05:35 -0500

    minor adjustments to analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,15 +19,16 @@ class MyDocument extends Document {
           {/* <!-- Google tag (gtag.js) --> */}
           <Script
             src={"https://www.googletagmanager.com/gtag/js?id=G-7C5JKWZTHL"}
+            strategy="afterInteractive"
           />
-          <Script id="google-analytics">
+          <Script id="google-analytics" strategy="afterInteractive">
             {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-7C5JKWZTHL');
-        `}
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'G-7C5JKWZTHL');
+            `}
           </Script>
           <Main />
           <NextScript />
